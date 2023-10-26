0d982e93bff5a3c8f9e9fcc7dc860bd718c3bac9
Bruno Caitano <snowrunescape@snowdev.com.br>
2023-10-26 19:53:14 -0400

    feat: effect on show and hide punishment of rule

--- src/components/Rule/index.tsx
+++ src/components/Rule/index.tsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { RuleProps } from './types';
 import { useTranslation } from 'react-i18next';
+import {
+  CSSTransition,
+} from 'react-transition-group';
+import './index.css';
 
 const Product = (props: RuleProps) => {
   const { t } = useTranslation();
 
-  const { rule, description, punishment } = props.rule;
+  const { id, rule, description, punishment } = props.rule;
 
   const [state, setState] = useState(false);
+
+  const nodeRef = useRef(null);
 
   return (
     <div className="flex flex-col items-center gap-2">
@@ -19,9 +25,18 @@ const Product = (props: RuleProps) => {
 
       <button className="btn btn-danger" onClick={() => setState(!state)}>{t("words.punishment")}</button>
 
-      {state && <div className="bg-gray-200 w-full max-w-[50%] p-3 rounded">
-        <span className="font-bold text-red-500 uppercase">{t("words.punishment")}:</span> {punishment}
-      </div>}
+      <CSSTransition
+        timeout={500}
+        classNames="rule-node"
+        in={state}
+        nodeRef={nodeRef}
+        key={id}
+        unmountOnExit
+      >
+        <div ref={nodeRef} className="bg-gray-200 w-full max-w-[50%] p-3 rounded">
+          <span className="font-bold text-red-500 uppercase">{t("words.punishment")}:</span> {punishment}
+        </div>
+      </CSSTransition>
     </div>
   );
 }
