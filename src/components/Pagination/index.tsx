0c679e29dad5f4b7617496e80cf28e429ae3c1c6
Bruno Caitano <snowrunescape@snowdev.com.br>
2024-03-29 09:30:23 -0400

    feat: melhorando o layout da paginação

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,22 +1,24 @@
 import { PaginationProps } from "./types";
 
 const Pagination = (props: PaginationProps) => {
-  const { total, itemsPerPage, updateCurrentPage } = props;
+  const { total, itemsPerPage, currentPage, updateCurrentPage } = props;
 
   const totalPages = Math.ceil(total / itemsPerPage);
 
   const getPageNumbers = () => {
-    const pageNumbers = [];
-    for (let i = 1; i <= totalPages; i++) {
-      pageNumbers.push(i);
-    }
-    return pageNumbers;
+    return Array.from({
+      length: totalPages
+    }, (_, index) => index + 1);
   };
 
   return (
     <div className="flex gap-3">
       {getPageNumbers().map(page => (
-        <button key={page} className="px-3 py-1 bg-gray-400" onClick={() => updateCurrentPage(page)}>
+        <button
+          key={page}
+          className={`px-3 py-1 border-solid border-2 border-primary rounded ${(currentPage == page) ? "bg-primary" : ""}`}
+          onClick={() => updateCurrentPage(page)}
+        >
           {page}
         </button>
       ))}
