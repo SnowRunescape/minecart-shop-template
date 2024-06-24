90205a6fe06a52d21d15d05c86799650416078ff
Bruno Caitano <snowrunescape@snowdev.com.br>
2024-06-23 21:34:21 -0400

    fix: use new variable from render username

--- src/components/TeamMember/index.tsx
+++ src/components/TeamMember/index.tsx
@@ -3,18 +3,18 @@ import { getHelmByUsername } from '@Minecart/helpers/minecraft';
 import { TeamMemberProps } from './types';
 
 const TeamMember = (props: TeamMemberProps) => {
-  const { name } = props.member;
+  const { username } = props.member;
 
   return (
     <div className="grid place-items-center p-3 gap-2 bg-gray-100 rounded">
       <Img
-        src={getHelmByUsername(name)}
+        src={getHelmByUsername(username)}
         className="rounded"
         effect="opacity"
         placeholderSrc="https://cdn.minecart.com.br/assets/img/steve_head_64x64.png"
       />
 
-      <div className="font-bold">{name}</div>
+      <div className="font-bold">{username}</div>
     </div>
   )
 }
