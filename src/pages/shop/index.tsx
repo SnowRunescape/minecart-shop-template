44bfd74548cc295f7477806be09e39fa868f2e5f
Bruno Caitano <snowrunescape@snowdev.com.br>
2024-09-08 08:10:46 -0400

    feat: categories render

--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -3,27 +3,45 @@ import CategoriesBar from "@Minecart/components/CategoriesBar";
 import NotFound from "@Minecart/components/NotFound";
 import ProductComponent from "@Minecart/components/product";
 import useDocumentTitle from "@Minecart/hooks/useDocumentTitle";
+import useSideBar from "@Minecart/hooks/useSideBar";
 import { useGetCategories } from "@Minecart/services/categories";
 import { useGetProducts } from "@Minecart/services/products";
+import { useMemo } from "react";
 import { useParams } from "react-router-dom";
 
 const Servers = () => {
   useDocumentTitle("Loja");
 
-  const { data: products, isLoading: isProductsLoading } = useGetProducts();
-  const { data: categories, isLoading: isCategoriesLoading } =
+  const { data: productsData, isPending: isProductsPending } = useGetProducts();
+  const { data: categoriesData, isPending: isCategoriesPending } =
     useGetCategories();
   const { server } = useParams();
 
-  if (isProductsLoading || isCategoriesLoading) {
+  const products = useMemo(() => {
+    if (!productsData) {
+      return [];
+    }
+
+    return productsData?.filter((product) => product.shop_server == server);
+  }, [productsData]);
+
+  const categories = useMemo(() => {
+    if (!categoriesData) {
+      return [];
+    }
+
+    return categoriesData?.filter((category) => {
+      return products?.find((product) => product.category == category.id);
+    });
+  }, [categoriesData]);
+
+  useSideBar(!categories?.length);
+
+  if (isProductsPending || isCategoriesPending) {
     return "carregando";
   }
 
-  const productsFiltered = products?.filter(
-    (product) => product.shop_server == server
-  );
-
-  if (!productsFiltered?.length) {
+  if (!products?.length) {
     return (
       <NotFound
         title="Nenhum produto publicada"
@@ -34,11 +52,13 @@ const Servers = () => {
 
   return (
     <div className="flex gap-3">
-      {!!categories.length && <CategoriesBar categories={categories} />}
+      {categories && categories?.length > 0 && (
+        <CategoriesBar categories={categories} />
+      )}
 
       <Card>
         <div className="grid grid-cols-2 w-full gap-4">
-          {productsFiltered.map((product) => (
+          {products.map((product) => (
             <ProductComponent key={product.id} product={product} />
           ))}
         </div>
