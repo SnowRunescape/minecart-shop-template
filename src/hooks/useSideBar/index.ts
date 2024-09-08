0f06265f9da992335f40565ad29a0ea09ad3dcf1
Bruno Caitano <snowrunescape@snowdev.com.br>
2024-09-08 11:01:50 -0400

    fix: use layout effect

--- src/hooks/useSideBar/index.ts
+++ src/hooks/useSideBar/index.ts
@@ -1,10 +1,10 @@
 import { SideBarContext } from "@Minecart/contexts/providers/SideBar";
-import { useContext, useEffect } from "react";
+import { useContext, useLayoutEffect } from "react";
 
 const useSideBar = (visible: boolean) => {
   const { setIsSideBarVisible } = useContext(SideBarContext);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     setIsSideBarVisible(visible);
   });
 };
